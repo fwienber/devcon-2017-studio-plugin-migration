03e1c19e5ad588b34b35dad37a036d4673ec2ebb
fwienber <frank.wienberg@coremedia.com>
2017-05-03 14:27:17 +0200

    Convert *.module.js to package.json
    ...and move custom CSS as configuration into pom.xml.
    Resources must be copied to a certain output directory of the module
    (package) so that Ext can load them.
    Removing *.module.js makes a special filtering resource rule
    in pom.xml obsolete. And there are no more generated-resources,
    so that resource rule can be removed, too.

--- rubber-duck-studio-plugin/src/main/resources/META-INF/resources/joo/rubber-duck-studio-plugin.module.js
+++
@@ -1,6 +0,0 @@
-joo.loadModule("${project.groupId}", "${project.artifactId}");
-joo.loadStyleSheet("joo/resources/css/rubber-duck.css");
-coremediaEditorPlugins.push({
-  "name": "Rubber Duck Plugin",
-  "mainClass": "com.acme.RubberDuckStudioPlugin"
-});
